f42fa4fb1ca0b6bbaf9d8eb7f9d15b938e5a7def
Moody Al-Oyouni <moody@macbook-pro-alkhas-b-moody.local>
2017-12-19 11:23:12 +0300

    Data persisted using UserDefaults methods.

--- Todo/TodoListViewController.swift
+++ Todo/TodoListViewController.swift
@@ -12,9 +12,15 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Moody", "Distroy the universe", "eat your breakfast"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
     
     //MARK - TableView Datasourse Methods
@@ -61,6 +67,8 @@ class TodoListViewController: UITableViewController {
             //What will happen once the user click the Add Item button on our UIAlert
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
             
